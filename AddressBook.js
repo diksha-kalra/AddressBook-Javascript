f29fb4cd4c7ed1c21c9c0cd5a238c0305f749264
diksha-kalra <kalradiksha11@gmail.com>
2020-11-10 22:44:09 +0530

    Added: validating contact details using regex patterns

--- AddressBook.js
+++ AddressBook.js
@@ -1,3 +1,10 @@
+//constants
+const NAME_REGEX_PATTERN=RegExp('^[A-Z]{1}[a-z]{2,}$');
+const ADDRESS_REGEX_PATTERN=RegExp('^[a-zA-z]{4,}$');
+const PINCODE_REGEX_PATTERN=RegExp('^[1-9]{1}[0-9]{5}|[1-9]{1}[0-9]{2}\\s[0-9]{3}');
+const PHONE_NUMBER_PATTERN = RegExp('^[1-9]{2}\\s[1-9]{1}[0-9]{9}$');
+const EMAIL_REGEX_PATTERN=RegExp(/[a-zA-Z0-9]+([._+-][0-9a-zA-Z])*@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+
 class ContactPersonData{
     //property
     firstName;
@@ -11,14 +18,38 @@ class ContactPersonData{
 
     //constructor
     constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.address=address;
-        this.city=city;
-        this.state=state;
-        this.zip=zip;
-        this.phoneNumber=phoneNumber;
-        this.email=email
+        if(!NAME_REGEX_PATTERN.test(firstName)) throw "invalid first name"
+        {
+            this.firstName=firstName;
+        }
+        if(!NAME_REGEX_PATTERN.test(lastName)) throw "invalid last name"
+        {
+            this.lastName=lastName;
+        }
+        if(!ADDRESS_REGEX_PATTERN.test(address)) throw "invalid address"
+        {
+            this.address=address;
+        }
+        if(!ADDRESS_REGEX_PATTERN.test(city)) throw "invaid city name"
+        {
+            this.city=city;
+        }
+        if(!ADDRESS_REGEX_PATTERN.test(state)) throw "invalid state name"
+        {
+            this.state=state;
+        }
+        if(!PINCODE_REGEX_PATTERN.test(zip)) throw "invalid zip code"
+        {
+            this.zip=zip;
+        }
+        if(!PHONE_NUMBER_PATTERN.test(phoneNumber)) throw "invalid phone number"
+        {
+            this.phoneNumber=phoneNumber;
+        }
+        if(!EMAIL_REGEX_PATTERN.test(email)) throw "invalid email address"
+        {
+            this.email=email
+        } 
     }
     
     //method
@@ -28,5 +59,9 @@ class ContactPersonData{
     }
 }
 
-let person=new ContactPersonData("diksha","kalra","delhi","delhi","delhi",110019,9899161947,"kalra@gmail.com");
-console.log(person.toString());
+try{
+    let person=new ContactPersonData("Diksha","Kalra","delhi","delhi","delhi",110019,"91 9899161947","kalra@gmail.com");
+    console.log(person.toString());
+}catch(e){
+    console.log(e);
+}
