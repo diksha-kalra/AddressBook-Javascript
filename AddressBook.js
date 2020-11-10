07e7cb57b9f6e02f628c3db52bc8c4abde2d31b9
diksha-kalra <kalradiksha11@gmail.com>
2020-11-10 22:04:43 +0530

    Added: creating address book contact

---
+++ AddressBook.js
@@ -0,0 +1,32 @@
+class ContactPersonData{
+    //property
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    //constructor
+    constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phoneNumber=phoneNumber;
+        this.email=email
+    }
+    
+    //method
+    toString(){
+        return "First Name: "+this.firstName+" Last Name: "+this.lastName+" Address: "+this.address+" City: "+this.city+
+        " State: "+this.state+" Zip: "+this.zip+" Phone Number: "+this.phoneNumber+" Email: "+this.email;
+    }
+}
+
+let person=new ContactPersonData("diksha","kalra","delhi","delhi","delhi",110019,9899161947,"kalra@gmail.com");
+console.log(person.toString());
